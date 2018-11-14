b3452c5342d14503bf5523319b206f35207b99ad
Artem Valkov <valkov.artyom@gmail.com>
2018-11-14 12:19:45 +0200

    eslint

--- gulpfile.js
+++ gulpfile.js
@@ -4,77 +4,76 @@ const clean = require('gulp-clean');
 const concat = require('gulp-concat');
 const runSequence = require('run-sequence');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 
 const distDirectory = 'dist';
 const htmlBlob = 'src/*.html';
 const imagesBlob = 'src/images/**';
 const fontsBlob = 'src/fonts/**';
 const stylesBlob = 'src/css/**';
+const sassBlob = 'src/scss/**';
+const outSass = 'dist/css/';
+const htmlTemp = 'src/**/*.html';
+const jsBlob = 'src/js/*.js';
 
-gulp.task('default', function () {
-  return runSequence('build', 'serve');
-});
+gulp.task('start', () => runSequence('build', 'serve'));
 
-gulp.task('build', function () {
-  return runSequence(
-    'cleanDist',
-    ['processStyles', 'processHtml', 'processImages', 'processFonts']
-  );
-});
+gulp.task('build', () => runSequence(
+  'cleanDist',
+  ['processStyles', 'processHtml', 'processImages', 'processFonts', 'sass', 'htmlTemp', 'processJs'],
+));
 
-gulp.task('serve', function () {
+gulp.task('serve', () => {
   browserSync.init({
     server: {
-      baseDir: distDirectory
-    }
+      baseDir: distDirectory,
+    },
   });
 
-  gulp.watch(htmlBlob, function () {
-    return runSequence('processHtml', 'reloadBrowser');
-  });
+  gulp.watch(htmlBlob, () => runSequence('processHtml', 'reloadBrowser'));
 
-  gulp.watch(imagesBlob, function () {
-    return runSequence('processImages', 'reloadBrowser');
-  });
+  gulp.watch(jsBlob, () => runSequence('processJs', 'reloadBrowser'));
 
-  gulp.watch(fontsBlob, function () {
-    return runSequence('processFonts', 'reloadBrowser');
-  });
+  gulp.watch(htmlTemp, () => runSequence('htmlTemp', 'reloadBrowser'));
 
-  gulp.watch(stylesBlob, function () {
-    return runSequence('processStyles', 'reloadBrowser');
-  });
+  gulp.watch(imagesBlob, () => runSequence('processImages', 'reloadBrowser'));
+
+  gulp.watch(fontsBlob, () => runSequence('processFonts', 'reloadBrowser'));
+
+  gulp.watch(stylesBlob, () => runSequence('processStyles', 'reloadBrowser'));
+
+  gulp.watch(sassBlob, () => runSequence('sass', 'reloadBrowser'));
 });
 
-gulp.task('cleanDist', function () {
-  return gulp.src(distDirectory, {read: false, allowEmpty: true}).pipe(clean());
-});
+gulp.task('cleanDist', () => gulp.src(distDirectory, { read: false, allowEmpty: true }).pipe(clean()));
 
-gulp.task('processHtml', function () {
-  return gulp.src(htmlBlob)
-    .pipe(gulp.dest(distDirectory));
-});
+gulp.task('processHtml', () => gulp.src(htmlBlob)
+  .pipe(gulp.dest(distDirectory)));
 
-gulp.task('processImages', function () {
-  return gulp.src(imagesBlob)
-    .pipe(gulp.dest(`${distDirectory}/images/`));
-});
+gulp.task('processJs', () => gulp.src(jsBlob)
+  .pipe(gulp.dest(distDirectory)));
 
-gulp.task('processFonts', function () {
-  return gulp.src(fontsBlob)
-    .pipe(gulp.dest(`${distDirectory}/fonts/`));
-});
+gulp.task('htmlTemp', () => gulp.src(htmlTemp)
+  .pipe(gulp.dest(distDirectory)));
 
-gulp.task('processStyles', function () {
-  return gulp.src(stylesBlob)
-    .pipe(concat('styles.css'))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
-    .pipe(gulp.dest(`${distDirectory}/css`));
-});
+gulp.task('processImages', () => gulp.src(imagesBlob)
+  .pipe(gulp.dest(`${distDirectory}/images/`)));
 
-gulp.task('reloadBrowser', function (done) {
+gulp.task('processFonts', () => gulp.src(fontsBlob)
+  .pipe(gulp.dest(`${distDirectory}/fonts/`)));
+
+gulp.task('processStyles', () => gulp.src(stylesBlob)
+  .pipe(concat('styles.css'))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+  }))
+  .pipe(gulp.dest(`${distDirectory}/css`)));
+
+gulp.task('sass', () => gulp.src(sassBlob)
+  .pipe(sass())
+  .pipe(gulp.dest(outSass)));
+
+gulp.task('reloadBrowser', (done) => {
   browserSync.reload();
   done();
 });
